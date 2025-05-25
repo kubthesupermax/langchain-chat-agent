76a7ba6428d03c8ae4d475ce8f7bde6d44055609
Franklin Kuba <franklinkuba7@gmail.com>
2025-05-24 19:25:57 -0500

    Chat without customized tools

---
+++ main.py
@@ -0,0 +1,29 @@
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    model = ChatOpenAI(temperature=0)
+    tools = []
+    
+    agent_executor = create_react_agent(model, tools)
+    print("Welcome! How can I help you today? Type 'quit' to exit.")
+    while True:
+        user_input = input("\nYou: ").strip()
+        if user_input == "quit":
+            break
+        print("\nAssistant: ", end="")
+        for chunk in agent_executor.stream({"messages": HumanMessage(content=user_input)}):
+            
+            if "agent" in chunk and "messages" in chunk["agent"]:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+        print()
+        
+if __name__ == "__main__":
+    main()
